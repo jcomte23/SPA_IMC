4a4eecdb0c411e0b96edaf5cc85b7b76cea46587
Javier Cómbita Téllez <javiercombita2014@gmail.com>
2025-06-16 19:44:13 -0500

    feat: the scritp for the imc was created in spanish

---
+++ src/js/main.js
@@ -0,0 +1,22 @@
+let peso
+let altura
+let imc
+
+peso = prompt("ingresa tu peso(kg)")
+altura = prompt("ingresa tu altura(metros)")
+
+imc = peso / (altura*altura)
+
+console.info("tu imc es "+ imc)
+
+// crear una tabla de clasificacion
+// codicionales
+
+if (condition) {
+    
+}else if(condition){}
+
+else{
+
+}
+
