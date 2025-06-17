7d70a7c965faea5512da8a606faaa3cf5a8b5fd1
Javier Cómbita Téllez <javiercombita2014@gmail.com>
2025-06-17 17:31:46 -0500

    fix:reset for the explication

--- src/js/main.js
+++ src/js/main.js
@@ -1,29 +1,16 @@
-const weight = document.getElementById("weight").value
-const height = document.getElementById("height").value
-const btnExecute = document.getElementById("btn-execute")
-const result = document.getElementById("result")
+const weight = prompt("ingrese tu peso")
+const height = prompt("ingrese tu altura en metros")
 
+const imc = weight / (height * height)
 
-btnExecute.addEventListener('click', function () {
-    const imc = weight / (height * height)
+let message
 
-    let message
-
-    if (imc < 18.5) {
-        message = 'Bajo peso';
-    } else if (imc < 24.9) {
-        message = 'Peso normal';
-    } else if (imc < 29.9) {
-        message = 'Sobrepeso';
-    } else {
-        message = 'Obesidad';
-    }
-
-    result.textContent = `estas con ${message} y tu imc es de ${Math.round(imc) }`
-})
-
-
-
-
-
-
+if (imc < 18.5) {
+    message = 'Bajo peso';
+} else if (imc < 24.9) {
+    message = 'Peso normal';
+} else if (imc < 29.9) {
+    message = 'Sobrepeso';
+} else {
+    message = 'Obesidad';
+}
