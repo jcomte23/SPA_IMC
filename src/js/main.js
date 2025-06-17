a548bb71ecc31232580d88e7166eb2c8f84de78e
Javier Cómbita Téllez <javiercombita2014@gmail.com>
2025-06-17 18:33:32 -0500

    feat: the firts version was finish

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,25 @@
-const weight = prompt("ingrese tu peso")
-const height = prompt("ingrese tu altura en metros")
+const weight = document.getElementById("weight")
+const height = document.getElementById("height")
+const btnExecute = document.getElementById("btn-execute")
+const result = document.getElementById("result")
 
-const imc = weight / (height * height)
 
-let message
+btnExecute.addEventListener('click', function () {
 
-if (imc < 18.5) {
-    message = 'Bajo peso';
-} else if (imc < 24.9) {
-    message = 'Peso normal';
-} else if (imc < 29.9) {
-    message = 'Sobrepeso';
-} else {
-    message = 'Obesidad';
-}
+    const imc = weight.value / (height.value * height.value)
+
+    let message
+
+    if (imc < 18.5) {
+        message = 'Bajo peso';
+    } else if (imc < 24.9) {
+        message = 'Peso normal';
+    } else if (imc < 29.9) {
+        message = 'Sobrepeso';
+    } else {
+        message = 'Obesidad';
+    }
+
+    result.textContent="tu tienes un imc de "+ Math.round(imc) +" por lo tanto esta en: "+ message
+})
+
