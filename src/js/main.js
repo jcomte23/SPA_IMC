d4094cdfe53490faa451427329ad6eb87e46ef76
Javier Cómbita Téllez <javiercombita2014@gmail.com>
2025-06-17 14:41:26 -0500

    fix: the firts version is finished

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,29 @@
-const weight = 12
-const height = 12
+const weight = document.getElementById("weight").value
+const height = document.getElementById("height").value
+const btnExecute = document.getElementById("btn-execute")
+const result = document.getElementById("result")
 
-const imc = weight / (height * height)
 
-let message
+btnExecute.addEventListener('click', function () {
+    const imc = weight / (height * height)
 
-if (imc < 18.5) {
-    message = 'Bajo peso';
-} else if (imc < 24.9) {
-    message = 'Peso normal';
-} else if (imc < 29.9) {
-    message = 'Sobrepeso';
-} else {
-    message = 'Obesidad';
-}
+    let message
 
-result.textContent = `Tu IMC es ${imc.toFixed(2)} - ${message}`;
+    if (imc < 18.5) {
+        message = 'Bajo peso';
+    } else if (imc < 24.9) {
+        message = 'Peso normal';
+    } else if (imc < 29.9) {
+        message = 'Sobrepeso';
+    } else {
+        message = 'Obesidad';
+    }
+
+    result.textContent = `estas con ${message} y tu imc es de ${Math.round(imc) }`
+})
+
+
+
+
+
+
